42325b5b8311c4d4c5fc6116bfaf7dcb71a1a449
Emelie Fäldt <emeliefaldt@hotmail.com>
2025-11-14 21:43:40 +0100

    add receiveMessage method and update sendMessage method

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.github.cdimascio.dotenv.Dotenv;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -17,11 +23,19 @@ public class HelloModel {
 
         private final String hostName;
         private final HttpClient http = HttpClient.newHttpClient();
+        private final ObjectMapper mapper = new ObjectMapper();
+
+        private final ObservableList<NtfyMessageDto> messages = FXCollections.observableArrayList();
 
         public HelloModel() {
             Dotenv dotenv = Dotenv.load();
             hostName = Objects.requireNonNull(dotenv.get("HOST_NAME"));
+            receiveMessage();
         }
+
+    public ObservableList<NtfyMessageDto> getMessages() {
+        return messages;
+    }
 
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
@@ -36,6 +50,7 @@ public class HelloModel {
 
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString("Hello World"))
+                .header("Cache", "no")
                 .uri(URI.create(hostName + "/mytopic"))
                 .build();
         try {
@@ -48,4 +63,21 @@ public class HelloModel {
             System.out.println("Interrupted sending message");
         }
     }
+
+    public void receiveMessage() {
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(hostName + "/mytopic/json"))
+                .build();
+
+        http.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofLines())
+                .thenAccept(response-> response.body()
+                        .map(s->
+                            mapper.readValue(s, NtfyMessageDto.class))
+                        .filter(message-> message.event().equals("message"))
+                        .peek(System.out::println)
+                        .forEach(s->
+                                Platform.runLater(()-> messages.add(s))));
+    }
+
 }
