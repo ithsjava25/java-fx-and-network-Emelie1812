a721477ac4be6a7b8ef51affcd8927157d45082c
Emelie Fäldt <emeliefaldt@hotmail.com>
2025-11-14 19:21:06 +0100

    add sendMessage method

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,9 +1,28 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+
 /**
  * Model layer: encapsulates application data and business logic.
  */
 public class HelloModel {
+
+
+        private final String hostName;
+        private final HttpClient http = HttpClient.newHttpClient();
+
+        public HelloModel() {
+            Dotenv dotenv = Dotenv.load();
+            hostName = Objects.requireNonNull(dotenv.get("HOST_NAME"));
+        }
+
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
@@ -11,5 +30,22 @@ public class HelloModel {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
+    }
+
+    public void sendMessage() {
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString("Hello World"))
+                .uri(URI.create(hostName + "/mytopic"))
+                .build();
+        try {
+            var response = http.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException e) {
+            System.out.println("Error sending message");
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted sending message");
+        }
     }
 }
