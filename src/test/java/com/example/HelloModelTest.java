4da0fa3b9632ea8385f77a54eccf7e7e986d4ca5
Emelie Fäldt <emeliefaldt@hotmail.com>
2025-11-17 10:24:56 +0100

    add HelloModelTest

--- src/test/java/com/example/HelloModelTest.java
+++ src/test/java/com/example/HelloModelTest.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@WireMockTest
 class HelloModelTest {
 
     @Test
@@ -19,6 +23,20 @@ class HelloModelTest {
         model.sendMessage();
         // Assert   Then
         assertThat(spy.message).isEqualTo("Hello World");
+
     }
 
+    @Test
+    void sendMessageToFakeServer(WireMockRuntimeInfo wmRuntimeInfo) {
+        var con = new NtfyConnectionImpl("http://localhost:" + wmRuntimeInfo.getHttpPort());
+        var model = new HelloModel(con);
+        model.setMessageToSend("Hello World");
+        stubFor(post("/mytopic").willReturn(ok()));
+
+        model.sendMessage();
+
+        // Verify call made to server
+        verify(postRequestedFor(urlEqualTo("/mytopic"))
+                .withRequestBody(containing("Hello World")));
+    }
 }
